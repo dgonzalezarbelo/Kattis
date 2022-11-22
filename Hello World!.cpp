ff827f409279dedded4935fcec7d5700026a38ae
Daniel GA <dgonzarb@gmail.com>
2022-11-22 10:10:52 +0100

    Hello World!

---
+++ Hello World!.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <iomanip>
+#include <climits>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+#pragma warning (disable : 4996)
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+/*-------------------------------------*/
+
+int main() {
+	cout << "Hello World!";
+	return 0;
+}
