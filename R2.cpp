6d89c697b0f70b9bba8fd9913665796b95123ed1
Daniel GA <dgonzarb@gmail.com>
2022-11-22 10:22:02 +0100

    R2

---
+++ R2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <iomanip>
+#include <climits>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+#pragma warning (disable : 4996)
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+
+/*-------------------------------------*/
+
+int main() {
+	int r, s;
+	cin >> r >> s;
+	cout << 2 * s - r;
+	return 0;
+}
