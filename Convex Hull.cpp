8c557d2a03a4e3e1a4d2424229ec412e10bce38f
Daniel GA <dgonzarb@gmail.com>
2023-02-02 10:51:05 +0100

    Convex Hull

---
+++ Convex Hull.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <set>
+#include <iomanip>
+#include <climits>
+#include <cmath>
+#include <list>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <bitset>
+#include <algorithm>
+#pragma warning (disable : 4996)
+
+using namespace std;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<ii> vii;
+typedef long long ll;
+typedef vector<ll> vll;
+typedef vector<vi> vvi;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+/*-------------------------------------*/
+
+struct pt {
+    double x, y;
+};
+
+int orientation(pt a, pt b, pt c) {
+    double v = a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y);
+    if (v < 0) return -1; // clockwise
+    if (v > 0) return +1; // counter-clockwise
+    return 0;
+}
+
+bool cw(pt a, pt b, pt c, bool include_collinear) {
+    int o = orientation(a, b, c);
+    return o < 0 || (include_collinear && o == 0);
+}
+bool collinear(pt a, pt b, pt c) { return orientation(a, b, c) == 0; }
+
+void convex_hull(vector<pt>& a, bool include_collinear = false) {
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+        });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x - a.x) * (p0.x - a.x) + (p0.y - a.y) * (p0.y - a.y)
+            < (p0.x - b.x) * (p0.x - b.x) + (p0.y - b.y) * (p0.y - b.y);
+        return o < 0;
+        });
+    if (include_collinear) {
+        int i = (int)a.size() - 1;
+        while (i >= 0 && collinear(p0, a[i], a.back())) i--;
+        reverse(a.begin() + i + 1, a.end());
+    }
+
+    vector<pt> st;
+    for (int i = 0; i < (int)a.size(); i++) {
+        while (st.size() > 1 && !cw(st[st.size() - 2], st.back(), a[i], include_collinear))
+            st.pop_back();
+        st.push_back(a[i]);
+    }
+
+    a = st;
+}
+
+bool resuelveCaso() {
+    int n;
+    cin >> n;
+    if (!n) return false;
+    vector<pt> a;
+    while (n--) {
+        pt p;
+        cin >> p.x >> p.y;
+        a.push_back(p);
+    }
+    convex_hull(a);
+    pt p0 = *min_element(a.begin(), a.end(), [](pt a, pt b) {
+        return make_pair(a.y, a.x) < make_pair(b.y, b.x);
+        });
+    sort(a.begin(), a.end(), [&p0](const pt& a, const pt& b) {
+        int o = orientation(p0, a, b);
+        if (o == 0)
+            return (p0.x - a.x) * (p0.x - a.x) + (p0.y - a.y) * (p0.y - a.y)
+            > (p0.x - b.x) * (p0.x - b.x) + (p0.y - b.y) * (p0.y - b.y);
+        return o > 0;
+        });
+    vector<pt> sol;
+    sol.push_back(a[0]);
+    for (int i = 1; i < a.size(); i++) {
+        if (a[i].x != a[i - 1].x || a[i].y != a[i - 1].y) {
+            sol.push_back(a[i]);
+        }
+    }
+    cout << sol.size() << '\n';
+    for (pt p : sol) {
+        cout << p.x << ' ' << p.y << '\n';
+    }
+    return true;
+}
+
+int main() {
+    while (resuelveCaso());
+    return 0;
+}
